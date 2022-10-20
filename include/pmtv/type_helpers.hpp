284e84bbb3f0cc11b2f83be289554ac44333632a
Josh Morman <jmorman@gnuradio.org>
2022-10-20 14:05:48 -0400

    Serialization for scalar and some basic vector
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>
    Signed-off-by: John Sallay <jasallay@gmail.com>

--- include/pmtv/type_helpers.hpp
+++ include/pmtv/type_helpers.hpp
@@ -1,6 +1,8 @@
 #include <complex>
 #include <concepts>
 #include <ranges>
+#include <variant>
+#include <vector>
 
 namespace pmtv {
 // It is really hard to declare a variant that contains itself.
@@ -17,8 +19,8 @@ using self_variant_base =
     nullptr_t,
     Ts...,
     std::shared_ptr<std::vector<Ts>>...,
-    std::shared_ptr<std::vector<self_variant<Ts...>>>,
-    std::shared_ptr<std::map<std::string, self_variant<Ts...>>>,
+    // std::shared_ptr<std::vector<self_variant<Ts...>>>,
+    // std::shared_ptr<std::map<std::string, self_variant<Ts...>>>,
     std::string
   >;
 
@@ -30,6 +32,7 @@ struct self_variant:
   using self_variant_base<Ts...>::self_variant_base;
   self_variant_base<Ts...> const& base() const { return *this; }
   self_variant_base<Ts...>& base() { return *this; }
+
 };
 
 
@@ -40,11 +43,16 @@ using _pmt_storage = self_variant<
     float, double, std::complex<float>, std::complex<double>
 >;
 
+
+
 using check2 = std::variant<
     uint8_t, uint16_t, uint32_t, uint64_t,
     int8_t, int16_t, int32_t, int64_t,
     float, double, std::complex<float>, std::complex<double>
 >;
+
+template <typename T>
+concept PmtNull = std::is_same_v<T, nullptr_t>;
 
 template <typename T>
 concept Complex = std::is_same_v<T, std::complex<float>> || std::is_same_v<T, std::complex<double>>;
@@ -61,6 +69,9 @@ concept PmtMap = std::is_same_v<T, std::map<std::string, _pmt_storage>>;
 template <typename T>
 concept PmtVector = std::is_same_v<T, std::vector<_pmt_storage>>;
 
+template <typename T> struct is_shared_ptr : std::false_type {};
+template <typename T> struct is_shared_ptr<std::shared_ptr<T>> : std::true_type {};
+template <typename T> concept IsSharedPtr = is_shared_ptr<T>::value;
 
 template <Scalar T>
 std::string type_string() {
@@ -79,9 +90,71 @@ std::string type_string() {
     return "Unknown Type";
 }
 
+enum class pmt_element_type : uint8_t {
+  UNKNOWN = 0,
+  UINT8 = 1,
+  UINT16,
+  UINT32,
+  UINT64,
+  INT8,
+  INT16,
+  INT32,
+  INT64,
+  FLOAT,
+  DOUBLE,
+  COMPLEX_FLOAT,
+  COMPLEX_DOUBLE,
+
+  STRING=253,
+  PMT=254,
+  INVALID=255
+};
+
+template <Scalar T>
+pmt_element_type element_type() {
+    if constexpr(std::is_same_v<T, uint8_t>) return pmt_element_type::UINT8;
+    else if constexpr(std::is_same_v<T, uint16_t>) return pmt_element_type::UINT16;
+    else if constexpr(std::is_same_v<T, uint32_t>) return pmt_element_type::UINT32;
+    else if constexpr(std::is_same_v<T, uint64_t>) return pmt_element_type::UINT64;
+    else if constexpr(std::is_same_v<T, int8_t>) return pmt_element_type::INT8;
+    else if constexpr(std::is_same_v<T, int16_t>) return pmt_element_type::INT16;
+    else if constexpr(std::is_same_v<T, int32_t>) return pmt_element_type::INT32;
+    else if constexpr(std::is_same_v<T, int64_t>) return pmt_element_type::INT64;
+    else if constexpr(std::is_same_v<T, float>) return pmt_element_type::FLOAT;
+    else if constexpr(std::is_same_v<T, double>) return pmt_element_type::DOUBLE;
+    else if constexpr(std::is_same_v<T, std::complex<float>>) return pmt_element_type::COMPLEX_FLOAT;
+    else if constexpr(std::is_same_v<T, std::complex<double>>) return pmt_element_type::COMPLEX_DOUBLE;
+    return pmt_element_type::UNKNOWN;
+}
+
+
+enum class pmt_container_type : uint16_t {
+    EMPTY,
+    SCALAR,
+    UNIFORM_VECTOR,
+    PMT_VECTOR,
+    MAP,
+    STRING
+};
+
+template <Scalar T>
+pmt_container_type container_type() {
+    return pmt_container_type::SCALAR;
+}
+
 template <UniformVector T>
 std::string type_string() {
     return "vector:" + type_string<typename T::value_type>();
+}
+
+template <UniformVector T>
+pmt_element_type element_type() {
+    return element_type<typename T::value_type>();
+}
+
+template <UniformVector T>
+pmt_container_type container_type() {
+    return pmt_container_type::UNIFORM_VECTOR;
 }
 
 template <PmtVector T>
@@ -89,15 +162,46 @@ std::string type_string() {
     return "vector:pmt";
 }
 
+template <PmtVector T>
+pmt_element_type element_type() {
+    return pmt_element_type::PMT;
+}
+
+template <PmtVector T>
+pmt_container_type container_type() {
+    return pmt_container_type::PMT_VECTOR;
+}
+
 template <PmtMap T>
 std::string type_string() {
     return "map:pmt";
+}
+
+template <PmtMap T>
+pmt_element_type element_type() {
+    return pmt_element_type::PMT;
+}
+
+template <PmtMap T>
+pmt_container_type container_type() {
+    return pmt_container_type::MAP;
 }
 
 template <class T>
 std::string type_string() {
     return "Unknown";
 }
+
+template <class T>
+pmt_element_type element_type() {
+    return pmt_element_type::UNKNOWN;
+}
+
+template <class T>
+pmt_container_type container_type() {
+    return pmt_container_type::EMPTY;
+}
+
 
 inline std::string get_type_string(const auto& arg) {
     using T = std::decay_t<decltype(arg)>;
@@ -106,3 +210,8 @@ inline std::string get_type_string(const auto& arg) {
 
 } // namespace
 
+
+namespace std {
+    template <> struct variant_size<pmtv::_pmt_storage> : variant_size<pmtv::_pmt_storage::variant> {};   
+    // template <std::size_t I> struct variant_alternative<I, pmtv::_pmt_storage> : variant_alternative<I, pmtv::_pmt_storage::variant> {};
+}
--- lib/map.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <pmtf/map.hpp>
-#include <map>
-#include <flatbuffers/flatbuffers.h>
-
-namespace pmtf {
-
-}
-
