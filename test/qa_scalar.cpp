839cffe4207f31bc556ccf579492e4cc7c0d7c97
Josh Morman <jmorman@gnuradio.org>
2022-11-17 06:20:38 -0500

    ci: use EQ instead of TRUE
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- test/qa_scalar.cpp
+++ test/qa_scalar.cpp
@@ -6,120 +6,101 @@
  * SPDX-License-Identifier: LGPL-3.0
  *
  */
-#include <gtest/gtest.h>
 #include <complex>
+#include <gtest/gtest.h>
 
 #include <pmtv/pmt.hpp>
 #include <sstream>
 
 using namespace pmtv;
 
-using testing_types = ::testing::Types<
-                                        uint8_t,
-                                       int8_t,
-                                       uint16_t,
-                                       int16_t,
-                                       uint32_t,
-                                       int32_t,
-                                       uint64_t,
-                                       int64_t,
-                                       float,
-                                       double,
-                                       std::complex<float>,
-                                       std::complex<double>>;
+using testing_types =
+    ::testing::Types<uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t,
+                     uint64_t, int64_t, float, double, std::complex<float>,
+                     std::complex<double>>;
 
-template <typename T>
-class PmtScalarFixture : public ::testing::Test
-{
+template <typename T> class PmtScalarFixture : public ::testing::Test {
 public:
-    T get_value() { return T(4); }
-    T zero_value() { return T(0); }
+  T get_value() { return T(4); }
+  T zero_value() { return T(0); }
 };
-
 
 template <> float PmtScalarFixture<float>::get_value() { return 4.1; }
 template <> double PmtScalarFixture<double>::get_value() { return 4.1; }
 
 template <>
-std::complex<float> PmtScalarFixture<std::complex<float>>::get_value()
-{
-    return std::complex<float>(4.1, -4.1);
+std::complex<float> PmtScalarFixture<std::complex<float>>::get_value() {
+  return std::complex<float>(4.1, -4.1);
 }
 
 template <>
-std::complex<double> PmtScalarFixture<std::complex<double>>::get_value()
-{
-    return std::complex<double>(4.1, -4.1);
+std::complex<double> PmtScalarFixture<std::complex<double>>::get_value() {
+  return std::complex<double>(4.1, -4.1);
 }
 
 TYPED_TEST_SUITE(PmtScalarFixture, testing_types);
 
-TYPED_TEST(PmtScalarFixture, PmtScalarNull)
-{
-    // Should initialize to nullptr
-    pmt x; //{this->get_value()};
-    EXPECT_TRUE(x == nullptr);
+TYPED_TEST(PmtScalarFixture, PmtScalarNull) {
+  // Should initialize to nullptr
+  pmt x; //{this->get_value()};
+  EXPECT_TRUE(x == nullptr);
 }
 
-TYPED_TEST(PmtScalarFixture, PmtScalarConstruction)
-{
-    // We should be able to do:
-    // a = 4;
-    // a(4);
-    // a = pmt(4)
-    // a(pmt(4))
-    auto value = this->get_value();
-    // Init from value
-    auto a = value;
-    EXPECT_EQ(a == value, true);
+TYPED_TEST(PmtScalarFixture, PmtScalarConstruction) {
+  // We should be able to do:
+  // a = 4;
+  // a(4);
+  // a = pmt(4)
+  // a(pmt(4))
+  auto value = this->get_value();
+  // Init from value
+  auto a = value;
+  EXPECT_EQ(a, value);
 
-    // Copy Constructor
-    auto b = a;
-    EXPECT_EQ(b == value, true);
-    EXPECT_EQ(b == a, true);
+  // Copy Constructor
+  auto b = a;
+  EXPECT_EQ(b, value);
+  EXPECT_EQ(b, a);
 
-    // Assignment operator from pmt
-    pmt c(b);
-    EXPECT_EQ(c == value, true);
-    EXPECT_EQ(c == a, true);
+  // Assignment operator from pmt
+  pmt c(b);
+  EXPECT_EQ(c, value);
+  EXPECT_EQ(c, a);
 
-    // Assignment operator from value
-    pmt d(value);
-    EXPECT_EQ(d == value, true);
-    EXPECT_EQ(d == a, true);
-    EXPECT_EQ(value == d, true);
-   
-    pmt e = value;
-    EXPECT_EQ(a == e, true);
-    EXPECT_EQ(e == value, true);
-    EXPECT_EQ(e == b, true);
+  // Assignment operator from value
+  pmt d(value);
+  EXPECT_EQ(d, value);
+  EXPECT_EQ(d, a);
+  EXPECT_EQ(value, d);
+
+  pmt e = value;
+  EXPECT_EQ(a, e);
+  EXPECT_EQ(e, value);
+  EXPECT_EQ(e, b);
 }
 
 TYPED_TEST(PmtScalarFixture, PmtScalarValue) {
-    // Get the value, change the value
-    auto value = this->get_value();
-    pmt x(value);
-    EXPECT_EQ(x, value);
-    value *= 2;
-    x = value;
-    EXPECT_EQ(x, value);
-    //pmt e({{"abc", 123}, {"you and me", "baby"}});
-    pmt e(std::vector({4, 5, 6}));
+  // Get the value, change the value
+  auto value = this->get_value();
+  pmt x(value);
+  EXPECT_EQ(x, value);
+  value *= 2;
+  x = value;
+  EXPECT_EQ(x, value);
+  // pmt e({{"abc", 123}, {"you and me", "baby"}});
+  pmt e(std::vector({4, 5, 6}));
 }
-
-
 
 TYPED_TEST(PmtScalarFixture, PmtScalarPrint) {
-    // Send to string stream and make sure it works.
-    auto value = this->get_value();
-    pmt x(value);
-    std::stringstream ss;
-    std::stringstream ss_check;
-    ss << x;
-    ss_check << value;
-    EXPECT_EQ(ss.str(), ss_check.str());
+  // Send to string stream and make sure it works.
+  auto value = this->get_value();
+  pmt x(value);
+  std::stringstream ss;
+  std::stringstream ss_check;
+  ss << x;
+  ss_check << value;
+  EXPECT_EQ(ss.str(), ss_check.str());
 }
-
 
 // TYPED_TEST(PmtScalarFixture, PmtScalarSerialize) {
 //     // Serialize/Deserialize and make sure that it works
@@ -130,7 +111,6 @@ TYPED_TEST(PmtScalarFixture, PmtScalarPrint) {
 //     auto y = pmtv::deserialize(sb);
 //     EXPECT_EQ(value, y);
 // }
-
 
 /*TYPED_TEST(PmtScalarFixture, explicit_cast)
 {
@@ -153,7 +133,6 @@ TYPED_TEST(PmtScalarFixture, PmtScalarPrint) {
     // FIXME: doesn't throw yet because this is not detected
     // EXPECT_ANY_THROW(std::vector<int>(x));
 }*/
-
 
 // TYPED_TEST(PmtScalarFixture, base64)
 // {
