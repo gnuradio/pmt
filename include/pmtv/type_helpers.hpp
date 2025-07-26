add1dd9e3fe17ffe75757a95b8098c407df237a1
John Sallay <jasallay@gmail.com>
2025-07-26 09:31:33 -0400

    Add a spaceship operator to tensor???
    Signed-off-by: John Sallay <jasallay@gmail.com>

--- include/pmtv/type_helpers.hpp
+++ include/pmtv/type_helpers.hpp
@@ -79,6 +79,7 @@ class Tensor {
         return _data[calculate_index(indices)];
     }
 
+    auto operator<=>(Tensor<T> const&) const = default;
     constexpr bool operator==(const Tensor<T>& other ) const noexcept {
         return _extents == other._extents && _data == other._data;
     }
